9e7c7275bd20522be6f59f3c7aee88d68723e5f5
Samrat Mallick <samrat.mallick@webskitters.com>
2021-08-02 07:22:45 +0530

    init

---
+++ fifth.py
@@ -0,0 +1,6 @@
+def say_hi(name):
+    print("Hello "+name+" greetings of the day.")
+
+
+user = input("PLease enter your name. : ")
+say_hi(user)
---
+++ third.py
@@ -0,0 +1,8 @@
+from math import *
+c_name = "Samrat"
+print("My name is " + c_name + ".")
+c_name = 9
+print("Number " + str(sqrt(c_name)) + ".")
+c_name = input("Please enter a name: ")
+age = input("hi, "+c_name+" enter your age : ")
+print("You have entered " + c_name + ". Your age is " + age + ".")
